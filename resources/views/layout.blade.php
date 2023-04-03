685288a4eef84b3b0d2ca11ce1bca36adb63b41a
alordiel <alexander.vasilev@protonmail.com>
2023-04-03 16:40:05 +0300

    added layout

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Main</li>
+                <li>Rest</li>
+            </ul>
+        </nav>
+    </header>
+    
+    @yield('content');
+    
+    <footer>
+        <p style="text-align:center">all right reserved</p>
+    </footer>
+</body>
+</html>
