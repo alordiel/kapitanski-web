a6d81f8dfa2e1e6366f22c064b2e6d8723ab46ce
alordiel <alexander.vasilev@protonmail.com>
2023-04-20 17:35:11 +0300

    added edit option

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,25 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Product; 
+use App\Models\Product;
 use Validator;
 
 class ProductController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('product.index', [
             'products' => Product::all()
         ]);
     }
 
-    public function create() {
+    public function create()
+    {
         return view('product.create');
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $formFields = $request->validate([
             'product_name' => 'required',
-            'price' => ['required','numeric']
+            'price' => ['required', 'numeric']
         ]);
 
         Product::create($formFields);
@@ -29,11 +32,25 @@ class ProductController extends Controller
         return redirect('/products')->with('message', 'Product successfully created');
     }
 
-    public function show (Product $product) {
-        return view('product.show', ['product'=>$product]);
+    public function show(Product $product)
+    {
+        return view('product.show', ['product' => $product]);
     }
 
-    public function edit(Product $product) {
-        return view('product.edit',['product'=> $product]);
+    public function edit(Product $product)
+    {
+        return view('product.edit', ['product' => $product]);
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        $formFields = $request->validate([
+            'product_name' => 'required',
+            'price' => ['required'],
+        ]);
+
+        $product->update($formFields);
+
+        return back()->with('message', 'Product updated successfully!');
     }
 }
--- resources/views/product/edit.blade.php
+++ resources/views/product/edit.blade.php
@@ -1,13 +1,13 @@
 Creating a new product
 <x-layout>
     <h1>Update: {{$product->product_name}}</h1>
-    <form action="/products" method="POST">
+    <form action="/products/{{$product->id}}" method="POST">
         @csrf
         @method("PUT")
         <p>
             <label for="product-name">
                 Product name <br>
-                <input type="text" id="product-name" name="product_name" value={{$product->product_name)}}>
+                <input type="text" id="product-name" name="product_name" value={{$product->product_name}}>
             </label>
             @error('product_name')
                 <br>
@@ -18,8 +18,8 @@ Creating a new product
         <p>
             <label for="price">
                 Price <br>
-                <input type="number" min="1.00" placeholder="product price" id="price" name="price" value={{$product->price)}}>
-            </label> 
+                <input type="number" min="1.00" placeholder="product price" id="price" name="price" value={{$product->price}}>
+            </label>
             @error('price')
                 <br>
                 <small style="color:red">{{$message}}</small>
