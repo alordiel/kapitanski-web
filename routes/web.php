dd42ed1a03a9482cc4cdcf699b9f03581d287b78
alordiel <alexander.vasilev@protonmail.com>
2023-04-26 15:02:47 +0300

    added middleware for Role check

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/products',[ProductController::class,'index']);
+Route::get('/products',[ProductController::class,'index'])->middleware('role:editor');
 
 require __DIR__.'/auth.php';
