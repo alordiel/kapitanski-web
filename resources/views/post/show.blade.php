a7aecd330f9b1c6382383f268dc229f7dcb506a0
alordiel <alexander.vasilev@protonmail.com>
2023-05-03 15:10:13 +0300

    fixed the displaying of posts

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -1,14 +1,10 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Create new product') }}
+            {{$post->title}}
         </h2>
     </x-slot>
-    <h1>{{$post->title}}</h1>
-    <small>Price: {{$ost->price}}</small>
-    <span class="edit">
-        <a href="/posts/{{$post->id}}/edit" title="edit">
-            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75zM20.71 5.63l-2.34-2.34a.996.996 0 00-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83a.996.996 0 000-1.41z"></path></svg>
-        </a>
-    </span>
+    <div>
+        {!!  $post->content !!}
+    </div>
 </x-app-layout>
