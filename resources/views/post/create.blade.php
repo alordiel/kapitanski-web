b37cac8ce6ca90b9aa7299e75efe7ed36c4b6ffe
alordiel <alexander.vasilev@protonmail.com>
2023-05-03 14:53:35 +0300

    formatted the post-create view

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,36 +1,42 @@
 <x-app-layout>
+    <script src="https://cdn.ckeditor.com/ckeditor5/37.1.0/classic/ckeditor.js"></script>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Create new product') }}
+            {{ __('Create new post') }}
         </h2>
     </x-slot>
-    <h1>Create new post</h1>
     <form action="/posts" method="POST">
         @csrf
         <p>
-            <label for="post-name">
-                Product name <br>
-                <input type="text" id="post-name" name="post_name" value={{old("post_name")}}>
-            </label>
-            @error('post_name')
-            <br>
-            <small style="color:red">{{$message}}</small>
+            <x-input-label for="title" :value="__('Post title')"/>
+            <x-text-input class="block w-1/5" id="title" name="title" required value="{{old('title')}}"  type="text" />
+            @error('title')
+            <x-input-error :messages="$message" class="mt-2"/>
             @enderror
 
         </p>
         <p>
-            <label for="price">
-                Price <br>
-                <input type="number" min="1.00" placeholder="post price" id="price" name="price" value={{old("price")}}>
-            </label>
-            @error('price')
-            <br>
-            <small style="color:red">{{$message}}</small>
+            <x-input-label for="slug" :value="__('Post slug')"/>
+            <x-text-input class="block w-1/5" type="text" id="slug" name="slug" required value="{{old('slug')}}" />
+            @error('slug')
+            <x-input-error :messages="$message" class="mt-2"/>
             @enderror
         </p>
-        <button type="submit">Create</button>
-        <input type="hidden" value="0" name="post_order">
-        <input type="hidden" value="Product description" name="description">
-        <input type="hidden" value="1" name="number_of_credits">
+        <div style="max-width: 800px" class="my-5">
+            <label for="editor">Post content</label><br>
+            <div class="text-black">
+                <textarea id="editor" name="content"></textarea>
+            </div>
+        </div>
+        <script>
+            ClassicEditor
+                .create(document.querySelector('#editor'))
+                .catch(error => {
+                    console.error(error);
+                });
+        </script>
+        <x-primary-button>Create</x-primary-button>
+        <input type="hidden" value="0" name="featured_image">
+        <input type="hidden" value="Product description" name="excerpt">
     </form>
 </x-app-layout>
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,20 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        hmr: {
+            protocol: 'ws',
+            https: false,
+            host: '192.168.56.56',
+        },
+        host: "192.168.56.56",
+        https: false,
+        watch: {
+          usePolling: true,
+        },
+    },
     plugins: [
+
         laravel({
             input: [
                 'resources/css/app.css',
