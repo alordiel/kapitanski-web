b08658c20472f5e205a2d1c91c450596b72d93a3
vagrant <vagrant@homestead>
2023-05-04 15:48:51 +0000

    changes in package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    server: {
-        hmr: {
-            protocol: 'ws',
-            https: false,
-            host: '192.168.56.56',
-        },
-        host: "192.168.56.56",
-        https: false,
-        watch: {
-          usePolling: true,
-        },
-    },
+
     plugins: [
 
         laravel({
