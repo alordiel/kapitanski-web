a312cffaeb39c96fe6e5aba7161281faf06f8901
alordiel <alexander.vasilev@protonmail.com>
2023-05-09 15:44:36 +0300

    buttons from edit/add posts for all-posts

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,9 +1,21 @@
 <x-app-layout>
     <script src="https://cdn.ckeditor.com/ckeditor5/37.1.0/classic/ckeditor.js"></script>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Create new post') }}
-        </h2>
+                <div class="flex justify-between">
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                {{ __('New posts') }}
+            </h2>
+            <a
+                href="/admin/posts"
+                class="flex items-center justify-between rounded bg-primary px-6 pb-2 pt-2.5 text-base font-bold text-white transition duration-150 ease-in-out hover:bg-primary-600  focus:bg-primary-600  focus:outline-none focus:ring-0 active:bg-primary-700 ">
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em"
+                     width="1em" xmlns="http://www.w3.org/2000/svg">
+                    <path fill="none" d="M0 0h24v24H0z"></path>
+                    <path d="M11.67 3.87L9.9 2.1 0 12l9.9 9.9 1.77-1.77L3.54 12z"></path>
+                </svg>
+                All Posts
+            </a>
+        </div>
     </x-slot>
     <form action="/admin/posts" method="POST">
         @csrf
