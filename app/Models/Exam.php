bec6711e82131ec8ea389e0efdb712826cbd3039
alordiel <alexander.vasilev@protonmail.com>
2023-05-10 11:48:42 +0300

    [exam] added relation to questions and removed 'category' column

--- app/Models/Exam.php
+++ app/Models/Exam.php
@@ -11,14 +11,20 @@ class Exam extends Model
     use HasFactory;
 
     protected bool $timestamp = false;
-    protected $fillable = ['name', 'category'];
+    protected $fillable = ['name'];
 
     public function examTaking(): hasMany
     {
         return $this->hasMany(ExamTaking::class);
     }
 
-    public function subscription(): hasMany{
+    public function subscription(): hasMany
+    {
         return $this->hasMany(Subscription::class);
+    }
+
+    public function question(): hasMany
+    {
+        return $this->hasMany(Question::class);
     }
 }
--- app/Models/Question.php
+++ app/Models/Question.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Question extends Model
@@ -34,5 +35,9 @@ class Question extends Model
     public function correctAnswer(): hasMany
     {
         return $this->hasMany(Answer::class);
+    }
+
+    public function exam(): belongsTo {
+        return $this->belongsTo(Exam::class);
     }
 }
