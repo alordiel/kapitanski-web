74715a83a38635e381f9a9ace93f8b1d1387751b
Aleksandar Vasilev <alexander.vasilev@protonmail.com>
2023-05-15 10:12:56 +0300

    added delete option for usres

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,26 +1,27 @@
 <x-app-layout>
     <x-slot name="header">
-        <div class="flex justify-between">
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                {{ __('All users') }}
-            </h2>
-            <a
-                href="/admin/users/create"
-                class="flex items-center justify-between rounded bg-primary px-6 pb-2 pt-2.5 text-base font-bold text-white transition duration-150 ease-in-out hover:bg-primary-600  focus:bg-primary-600  focus:outline-none focus:ring-0 active:bg-primary-700 ">
-                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" class="h-6 pr-2"
-                     xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        d="M854.6 288.6L639.4 73.4c-6-6-14.1-9.4-22.6-9.4H192c-17.7 0-32 14.3-32 32v832c0 17.7 14.3 32 32 32h640c17.7 0 32-14.3 32-32V311.3c0-8.5-3.4-16.7-9.4-22.7zM790.2 326H602V137.8L790.2 326zm1.8 562H232V136h302v216a42 42 0 0 0 42 42h216v494zM544 472c0-4.4-3.6-8-8-8h-48c-4.4 0-8 3.6-8 8v108H372c-4.4 0-8 3.6-8 8v48c0 4.4 3.6 8 8 8h108v108c0 4.4 3.6 8 8 8h48c4.4 0 8-3.6 8-8V644h108c4.4 0 8-3.6 8-8v-48c0-4.4-3.6-8-8-8H544V472z"></path>
-                </svg>
-                Add new
-            </a>
-        </div>
+        <x-subheader title="Add user" icon="add" button-text="Add user" url="{{route('user.admin.create')}}"/>
     </x-slot>
     <div>
         @foreach($users as $user)
             <p>
                 {{$user->name}} | {{$user->email}} | {{$user->getRoleNames()->first() }} |
-                <a href="/admin/users/{{$user->id}}/edit">(edit)</a> | <a href="/admin/users/{{$user->id}}">(view)</a> | <a>(delete)</a>
+                <a href="/admin/users/{{$user->id}}/edit">(edit)</a> | <a href="/admin/users/{{$user->id}}">(view)</a> |
+                <form method="POST" action="/admin/users/{{$user->id}}">
+                            @csrf
+                            @method('DELETE')
+                            <button class="delete" type="submit">
+                                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24"
+                                     height="1em"
+                                     width="1em" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill="none" d="M0 0h24v24H0z"></path>
+                                    <path
+                                        fill="none" d="M0 0h24v24H0V0z"></path>
+                                    <path
+                                        d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12 1.41 1.41L13.41 14l2.12 2.12-1.41 1.41L12 15.41l-2.12 2.12-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4z"></path>
+                                </svg>
+                            </button>
+                        </form>
             </p>
         @endforeach
     </div>
--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -1,19 +1,11 @@
 <x-app-layout>
     <x-slot name="header">
-        <div class="flex justify-between">
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                {{ __('Single user') }}
-            </h2>
-            <a
-                href="/admin/users/create"
-                class="flex items-center justify-between rounded bg-primary px-6 pb-2 pt-2.5 text-base font-bold text-white transition duration-150 ease-in-out hover:bg-primary-600  focus:bg-primary-600  focus:outline-none focus:ring-0 active:bg-primary-700 ">
-                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" class="h-6 pr-2"
-                     xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        d="M854.6 288.6L639.4 73.4c-6-6-14.1-9.4-22.6-9.4H192c-17.7 0-32 14.3-32 32v832c0 17.7 14.3 32 32 32h640c17.7 0 32-14.3 32-32V311.3c0-8.5-3.4-16.7-9.4-22.7zM790.2 326H602V137.8L790.2 326zm1.8 562H232V136h302v216a42 42 0 0 0 42 42h216v494zM544 472c0-4.4-3.6-8-8-8h-48c-4.4 0-8 3.6-8 8v108H372c-4.4 0-8 3.6-8 8v48c0 4.4 3.6 8 8 8h108v108c0 4.4 3.6 8 8 8h48c4.4 0 8-3.6 8-8V644h108c4.4 0 8-3.6 8-8v-48c0-4.4-3.6-8-8-8H544V472z"></path>
-                </svg>
-                All users
-            </a>
-        </div>
+        <x-subheader title="{{$user->name}}" icon="all" button-text="All users" url="{{route('user.admin.manage')}}"/>
     </x-slot>
+    <div>
+        <p>Name: {{$user->name}}</p>
+        <p>Email: {{$user->email}}</p>
+        <p>Role: {{$user->getRoleNames()->first()}}</p>
+        <p>Registered on: {{$user->created_at}}</p>
+    </div>
 </x-app-layout>
