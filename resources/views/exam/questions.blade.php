d3f54b891bd76f93b64a88b063dabde9267dee6d
alordiel <alexander.vasilev@protonmail.com>
2023-05-16 11:33:37 +0300

    [questions] support for image files

--- resources/views/exam/questions.blade.php
+++ resources/views/exam/questions.blade.php
@@ -10,21 +10,24 @@
         <div v-for="(question,key) in questions" :key="key" class="mb-3">
             <div class="flex flex-wrap justify-between mb-3">
                 <p class="w-1/3">
-                    <label class="block font-medium text-sm text-gray-700 dark:text-gray-300">@{{ key + 1 }}. Question body<br>
+                    <label class="block font-medium text-sm text-gray-700 dark:text-gray-300">@{{ key + 1 }}. Question
+                        body<br>
                         <x-text-input type="text" v-model="question.body"/>
                     </label>
                 </p>
                 <p class="w-1/3">
                     <label class="block font-medium text-sm text-gray-700 dark:text-gray-300">
                         Question type<br>
-                        <input type="radio" name="question-type" value="text"> Text <br>
-                        <input type="radio" name="question-type" value="image"> Image
+                        <input type="radio" :name="'question-type-' + key" v-model="question.type" value="text"> Text
+                        <br>
+                        <input type="radio" :name="'question-type-' + key" v-model="question.type" value="image"> Image
                     </label>
                 </p>
                 <p class="w-1/3">
-                    <label class="block font-medium text-sm text-gray-700 dark:text-gray-300">
+                    <label :for="'cat-' + key" class="block font-medium text-sm text-gray-700 dark:text-gray-300">
                         Question category <br>
-                        <select name="" id="">
+                        <select v-model="question.category" :id="'cat-' + key"
+                                class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">
                             <option value="0"> - - -</option>
                             <option value="1">Морски знаци</option>
                             <option value="2">Четене на карта</option>
@@ -36,7 +39,7 @@
             </div>
 
 
-            <div class="flex justify-between flex-wrap mb-3">
+            <div v-if="question.type !== 'image'" class="flex justify-between flex-wrap mb-3">
                 <p class="w-1/2">
                     <x-text-input type="text" placeholder="answer" class="w-full" v-model="question.asnwerA"/>
                 </p>
@@ -48,6 +51,20 @@
                 </p>
                 <p class="w-1/2">
                     <x-text-input type="text" placeholder="answer" class="w-full" v-model="question.asnwerD"/>
+                </p>
+            </div>
+            <div v-else class="flex justify-between flex-wrap mb-3">
+                <p class="w-1/2">
+                    <x-text-input type="file" class="w-full" v-model="question.asnwerA"/>
+                </p>
+                <p class="w-1/2">
+                    <x-text-input type="file" class="w-full" v-model="question.asnwerB"/>
+                </p>
+                <p class="w-1/2">
+                    <x-text-input type="file" class="w-full" v-model="question.asnwerC"/>
+                </p>
+                <p class="w-1/2">
+                    <x-text-input type="file" class="w-full" v-model="question.asnwerD"/>
                 </p>
             </div>
             <hr>
@@ -66,6 +83,7 @@
                             id: '',
                             body: '',
                             type: '',
+                            category: '',
                             answerA: '',
                             answerB: '',
                             answerC: '',
@@ -73,7 +91,6 @@
                             correctAnswer: ''
                         }
                     ],
-                    message: 'Hello Vue!'
                 }
             },
             methods: {
