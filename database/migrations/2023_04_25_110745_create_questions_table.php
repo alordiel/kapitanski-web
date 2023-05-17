975116e95d0210511f30c860ae2ae62e23478f50
Aleksandar Vasilev <alexander.vasilev@protonmail.com>
2023-05-17 11:16:04 +0300

    [questions] fix on the questions -> answers relations

--- database/migrations/2023_04_25_110745_create_questions_table.php
+++ database/migrations/2023_04_25_110745_create_questions_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->string('question');
-            $table->integer('answer_id');
             $table->integer('correct_answer');
             $table->integer('category_id');
+            $table->integer('exam_id');
             $table->integer('type');
         });
     }
--- database/migrations/2023_04_25_123253_create_answers_table.php
+++ database/migrations/2023_04_25_123253_create_answers_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('answers', function (Blueprint $table) {
             $table->id();
             $table->string('answer');
+            $table->integer('question_id');
         });
     }
 
