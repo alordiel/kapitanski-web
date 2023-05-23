3d0bcd4e861640afaf6a6ec8254fb6046bcaf216
Aleksandar Vasilev <alexander.vasilev@protonmail.com>
2023-05-23 10:47:36 +0300

    added payment card form to checkout page

--- resources/views/pages/checkout.blade.php
+++ resources/views/pages/checkout.blade.php
@@ -5,9 +5,8 @@
 
     <div class="w-2/4 mx-auto border rounded border-indigo-500">
         @auth
-            You are logged in
         @else
-            <div>
+            <div class="p-5">
                 <h3>Registration details</h3>
                 <div>
                     <x-input-label for="name" :value="__('Name')"/>
@@ -53,7 +52,49 @@
                         {{ __('Already having an account?') }}
                     </a>
                 </div>
+                <div class="border-b-indigo-500"></div>
             </div>
         @endauth
+        <div class="p-5">
+            <h3>Payment details</h3>
+            <div class="mt-4">
+                <x-input-label for="card-number" :value="__('Card number')"/>
+                <x-text-input id="card-number" class="block mt-1 w-full" type="text" name="card-number"
+                              :value="old('card-number')"
+                              required/>
+                <x-input-error :messages="$errors->get('card-number')" class="mt-2"/>
+            </div>
+            <div class="mt-4">
+                <x-input-label for="card-name" :value="__('Name on the card')"/>
+                <x-text-input id="card-name" class="block mt-1 w-full" type="text" name="card-name"
+                              :value="old('card-name')"
+                              required/>
+                <x-input-error :messages="$errors->get('card-name')" class="mt-2"/>
+            </div>
+            <div class="flex">
+                <div class="mt-4">
+                    <x-input-label for="card-date" :value="__('Expiration date')"/>
+                    <x-text-input id="card-date" class="block mt-1 w-full" type="text" name="card-date"
+                                  :value="old('card-date')"
+                                  required placeholder="mm/yy"/>
+                    <x-input-error :messages="$errors->get('card-date')" class="mt-2"/>
+                </div>
+                <div class="mt-4 ml-4">
+                    <x-input-label for="card-cvv" :value="__('CVV')"/>
+                    <x-text-input id="card-cvv" class="block mt-1 w-full" type="password" name="card-cvv"
+                                  :value="old('card-cvv')" max="3" min="3"
+                                  required/>
+                    <x-input-error :messages="$errors->get('card-cvv')" class="mt-2"/>
+                </div>
+
+            </div>
+            <div class="my-5 border border-indigo-500"></div>
+        </div>
+        <div>
+            <h3>Checkout details</h3>
+            <div class="flex justify-center">
+                <x-primary-button>Checkout</x-primary-button>
+            </div>
+        </div>
     </div>
 </x-app-layout>
