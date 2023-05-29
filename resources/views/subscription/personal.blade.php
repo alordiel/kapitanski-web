dcd1cb7f842c2f23975b97421e3f834117316e3d
Aleksandar Vasilev <alexander.vasilev@protonmail.com>
2023-05-29 17:50:11 +0300

    [subscriptions] fixed cases for parnters credits

--- resources/views/subscription/personal.blade.php
+++ resources/views/subscription/personal.blade.php
@@ -33,7 +33,7 @@
                             $expiration_date = $is_active ? __('Expires on') : __('Expired on');
                         @endphp
                         <div class="mb-5 mx-3 w-64 border rounded px-3 py-5">
-                            <p><strong>Order</strong> #{{$order->id}} </p>
+                            <p><strong>{{__('Order')}}:</strong> #{{$order->id}} </p>
                             <p><strong>{{__('Status')}}:</strong>
                                 <span
                                     class="{{ $is_active ? 'bg-green-400' : 'bg-red-500'}} inline-block text-white rounded px-3">
@@ -48,28 +48,55 @@
                             @endif
                         </div>
                     @elseif($order->credits > 1)
+                        {{-- Partners orders with many credits  --}}
                         <div class="mb-5 w-64 border p-5 mx-3 rounded dark:border-indigo-500">
-                            Your <strong>order #{{$order->id}}</strong> from {{date('d-m-Y', strtotime($order->created_at))}} is still not active. You
-                            can activate it from this <a href="{{route('my-exam')}}"> page</a>. <br>
-                            You can download your invoice <a href="#">here</a>.
+                            <p><strong>{{__('Order')}}:</strong> #{{$order->id}}</strong></p>
+                            <p><strong>{{__('Order status')}}:</strong> {{$order->order_status}}</strong></p>
+                            <p><strong>{{__('Order date')}}:</strong> {{date('d-m-Y', strtotime($order->created_at))}}</p>
+                            <p><strong>{{__('Total credits')}}:</strong> {{$order->credits}}</strong></p>
+                            <p><strong>{{__('Used credits')}}:</strong> {{$order->used_credits}}</strong></p>
                         </div>
                     @else
-                        has only order
+                        {{-- Order has only one credit and is not activated yet. We displays only the order  --}}
+                        <div class="mb-5 w-64 border p-5 mx-3 rounded dark:border-indigo-500">
+                            <p><strong>{{__('Order')}}:</strong> #{{$order->id}}</strong></p>
+                            <p><strong>{{__('Order status')}}:</strong> {{$order->order_status}}</strong></p>
+                            <p><strong>{{__('Order date')}}:</strong> {{date('d-m-Y', strtotime($order->created_at))}}
+                            </p>
+                            <p><strong>{{__('Invoice')}}:</strong> <a href="#">{{'Download'}}</a></p>
+                            <p>{{ __('Subscription is inactive.') }}</p>
+                            <p> {!! sprintf( __('Click <a href="%s">here</a> to activate.'), route('my-exam')) !!}</p>
+                        </div>
                     @endif
                 @endforeach
             </div>
         @elseif(!empty($subscriptions)  && count($subscriptions) > 0)
+            {{-- Covers the cases for students added by partner's organization--}}
             <div class="flex flex-wrap">
                 @foreach($subscriptions as $subscription)
+                    @php
+                        $created_on = date('d-m-Y', strtotime($subscription->created_at));
+                        $expires_on = date('d-m-Y', strtotime($subscription->expires_on));
+                        $is_active  = $expires_on > date("Y-m-d");
+                        $subscriptions_status = $is_active ? __('Active') : __('Expired');
+                        $expiration_date = $is_active ? __('Expires on') : __('Expired on');
+                    @endphp
                     <div>
-                        Exam: {{$subscription->exam->name}} |
-                        Started on: {{date('d-m-Y', strtotime($subscription->created_at))}} |
-                        Expires on: {{date('d-m-Y', strtotime($subscription->expires_on))}} |
-                        By: {{$subscription->createdBy->name}}
+                        <p><strong>{{__('Status')}}:</strong>
+                            <span
+                                class="{{ $is_active ? 'bg-green-400' : 'bg-red-500'}} inline-block text-white rounded px-3">
+                                {{ $subscriptions_status }}
+                            </span>
+                        </p>
+                        <p><strong>{{ __('Exam') }}:</strong> {{ $subscription->exam->name }} </p>
+                        <p><strong>{{ __('Started on') }}:</strong> {{ $created_on }} </p>
+                        <p><strong>{{ $expiration_date }}:</strong> {{ $expires_on }}</p>
+                        <p><strong>{{ __('Created by') }}:</strong> {{ $subscription->createdBy->name }}</p>
                     </div>
                 @endforeach
             </div>
         @else
+            {{-- No subscriptions, no orders --}}
             <h3 class="text-2xl text-center font-bold mb-5">{{__("You don't have any subscriptions yet.")}}</h3>
             <p class="text-center">{{__('If you account was created from some of our partners please contact them for further instructions.')}}</p>
             <p class="text-center">{!! sprintf(__("You can buy a new plan from <a href='%s' title='Buy a plan'>here</a>"),route('buy')) !!}</p>
