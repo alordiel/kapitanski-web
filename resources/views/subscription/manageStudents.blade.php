59d9946ba2c1b6e9dd8507f90d6b4727f8e49b4d
alordiel <alexander.vasilev@protonmail.com>
2023-05-31 11:49:16 +0300

    [subscriptions][students] added error fields for messages from the post request

--- resources/views/subscription/manageStudents.blade.php
+++ resources/views/subscription/manageStudents.blade.php
@@ -13,6 +13,10 @@
         <x-success-message message="{{session('message')}}"/>
     @endif
 
+    @error('message')
+        <x-input-error :messages="$message" class="my-4"/>
+    @enderror
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
