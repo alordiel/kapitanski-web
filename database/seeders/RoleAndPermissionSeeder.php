353c995cba4fa02a7af785bf7793e636d174532e
alordiel <alexander.vasilev@protonmail.com>
2023-06-01 15:00:07 +0300

    renamed the partner role to business-partner

--- database/seeders/RoleAndPermissionSeeder.php
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -44,7 +44,7 @@ class RoleAndPermissionSeeder extends Seeder
         $adminRole = Role::create(['name' => 'super-admin']);
         $activeMemberRole = Role::create(['name' => 'student']);
         $inactiveMemberRole = Role::create(['name' => 'member']);
-        $partnerRole = Role::create(['name' => 'partner']);
+        $partnerRole = Role::create(['name' => 'business-partner']);
         $studentPartnerRole = Role::create(['name' => 'student-partner']);
 
         $adminRole->givePermissionTo(Permission::all());
@@ -56,7 +56,7 @@ class RoleAndPermissionSeeder extends Seeder
         $user1 = User::where('id', 1)->get();
         $user1[0]->assignRole('super-admin');
         $user2 = User::where('id', 2)->get();
-        $user2[0]->assignRole('partner');
+        $user2[0]->assignRole('business-partner');
         $user3 = User::where('id', 3)->get();
         $user3[0]->assignRole('student');
     }
