9da565b6f23a15c1ce474f02615b0d406ccb9ef8
alordiel <alexander.vasilev@protonmail.com>
2023-07-05 10:23:03 +0300

    fixed the categories load

--- app/Repositories/QuestionRepository.php
+++ app/Repositories/QuestionRepository.php
@@ -15,14 +15,19 @@ class QuestionRepository extends Controller
     public static function getDemoQuestions(): Collection
     {
         $questions = new Question();
-        return $questions->answers()->where('exam_id', 2)->take(20)->get();
+        return $questions->answers()
+            ->where('exam_id', 2)
+            ->take(20)
+            ->get();
     }
 
     public function getQuestionsByCategory(int $category_id, int $count_of_questions): Collection
     {
-        $questions = new Question();
-        return $questions->answers()
-            ->where('question_category_id', $category_id)
+        return Question::where([
+            ['exam_id', '=', 1],
+            ['question_category_id','=', $category_id]
+            ])
+            ->with('answers')
             ->take($count_of_questions)
             ->get();
     }
