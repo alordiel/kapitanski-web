16933fb5674d4dee42508d92a9e5ce2283ea25a9
alordiel <alexander.vasilev@protonmail.com>
2023-07-12 15:55:24 +0300

    reset the storage folder; fixed the loading of the json in the seeder

--- database/seeders/Exam40BT.php
+++ database/seeders/Exam40BT.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class Exam40BT extends Seeder
 {
@@ -12,6 +13,13 @@ class Exam40BT extends Seeder
      */
     public function run(): void
     {
-        //
+        $questions = Storage::json('40BT.json');
+        $this->command->comment("is empty: " . empty($questions));
+
+        if(!empty($questions)) {
+            foreach ($questions as $question) {
+                $question_text = $question['questionText'];
+            }
+        }
     }
 }
